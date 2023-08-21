220afaadd4fb1268e10c9929a97e9a77721a5a71
Ivaylo Ivanov <ivaylo92ivanov@gmail.com>
2023-08-21 20:25:59 +0300

    Set font and create header nav

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./style.css";
+import restaurantBackground from "./restaurant-background-min.jpg"
 
+document.querySelector("").style.backgroundImage = `url(${restaurantBackground})`
+// document.querySelector(".main").style.backgroundColor = "black"
 
 
 console.log("whatever..")
